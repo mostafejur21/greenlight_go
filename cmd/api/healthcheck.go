c9327ceed7d21152860ea94d5f936dc37e90ea6e
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2025-01-15 21:50:10 +0600

    fix: json encoding add to the healthcheckHandler for encode the data map

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,12 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %v\n", app.config.env)
-	fmt.Fprintf(w, "version: %v\n", version)
+	// js := `{"status": "available", "environment": %q, "version": %q}`
+	// now create a map that will hold the apps information
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+    // By passing the data map to json.Marshal() function, this json.Marshal() function
+    // will return a []byte (slice of byte) and error.
+    // Why do we need Marshal() => to encode our GO code into JSON format
+    js, err := json.Marshal(data)
+    if err != nil {
+        app.logger.Error(err.Error())
+        http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+        return
+    }
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
 }
