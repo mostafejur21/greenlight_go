204d1588a081853ea1416900be4a9acce315ae77
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2025-01-16 00:15:14 +0600

    fix: movies struct encode json add and update the showMovieById Handler

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/mostafejur21/greenlight_go/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -10,14 +13,25 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-    id, err := app.readIDParam(r)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
 	}
 
-	//Otherwise we will show the response of that id
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "BatMan",
+		RunTime:   102,
+		Genres:    []string{"drama", "action", "war"},
+		Version:   1,
+	}
 
-	fmt.Fprintf(w, "show the details of the id %d\n", id)
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process the request", http.StatusInternalServerError)
+	}
 
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,19 @@
+package data
+
+import "time"
+
+// if we does not include the json annotation for the struct, the default struct value will
+// the json keys. like "ID", "CreatedAt" etc.
+// but if we include the struct annotation, then that will be the json keys.
+// like ID int64 `json:"id"`
+// Also note that the json annotation does not allow any space inside ``.
+// it will through warning and the annotation will not work
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"` // Use the '-' directive to hide some this field from the response
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"` // the omitempty will hide the field only if the value is empty
+	RunTime   int32     `json:"run_time,omitempty,string"` // if we add the string directive, the RunTime field will be shown as a string in the response
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
