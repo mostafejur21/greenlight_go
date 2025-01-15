2e93ca5e16f97fe074caad4b547459cd7bd9edfe
Mostafejur Rahman <mostafejur.smuct.cse@gmail.com>
2025-01-15 22:05:29 +0600

    fix: custom json response helper function add

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -13,16 +12,12 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"environment": app.config.env,
 		"version":     version,
 	}
-    // By passing the data map to json.Marshal() function, this json.Marshal() function
-    // will return a []byte (slice of byte) and error.
-    // Why do we need Marshal() => to encode our GO code into JSON format
-    js, err := json.Marshal(data)
-    if err != nil {
-        app.logger.Error(err.Error())
-        http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-        return
-    }
-
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(js)
+	// By passing the data map to json.Marshal() function, this json.Marshal() function
+	// will return a []byte (slice of byte) and error.
+	// Why do we need Marshal() => to encode our GO code into JSON format
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -9,12 +10,38 @@ import (
 )
 
 func (app *application) readIDParam(r *http.Request) (int64, error) {
-    params := httprouter.ParamsFromContext(r.Context())
+	// when httprouter parsing a request, any interpolated URL parameters will be
+	// stored in the request context. we can use the ParamsFromContext() function
+	// to retrive a slice containing this parameters names and values
+	params := httprouter.ParamsFromContext(r.Context())
 
-    id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	// use the ByName() methods to get the id.
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
 
-    if err != nil || id < 1 {
-        return 0, errors.New("invalid id params")
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id params")
+	}
+	return id, nil
+}
+
+func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+    // Encode the data into JSON, return error if there is any
+    js, err := json.Marshal(data)
+    if err != nil {
+        return err
     }
-    return id, nil
+
+    // append a new line to make it easy to read it in the terminal
+    js = append(js, '\n')
+
+    for key, value := range headers {
+        w.Header()[key] = value
+    }
+
+    // Adding the w.headers ("Content-type", "application/json")
+    w.Header().Set("Content-type", "application/json")
+    w.WriteHeader(status)
+    w.Write(js)
+
+    return nil
 }
